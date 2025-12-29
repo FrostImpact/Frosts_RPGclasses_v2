5e2f843ba9358c80e99d3dc8885fb0ded8c19e1e
FrostImpact <harrisonyaochip@gmail.com>
2025-12-29 10:42:19 -0500

    fixed class registry operation errors

--- src/main/java/net/frostimpact/rpgclasses_v2/rpgclass/ClassRegistry.java
+++ src/main/java/net/frostimpact/rpgclasses_v2/rpgclass/ClassRegistry.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class ClassRegistry {
     private static final Logger LOGGER = LoggerFactory.getLogger(ClassRegistry.class);
     private static final Map<String, RPGClass> classes = new HashMap<>();
-    
+
     /**
      * Register an RPG class
      */
@@ -27,56 +27,56 @@ public class ClassRegistry {
         classes.put(classId, rpgClass);
         LOGGER.debug("Registered class: {}", classId);
     }
-    
+
     /**
      * Get a class by ID
      */
     public static Optional<RPGClass> getClass(String classId) {
         return Optional.ofNullable(classes.get(classId));
     }
-    
+
     /**
      * Check if a class is registered
      */
     public static boolean isRegistered(String classId) {
         return classes.containsKey(classId);
     }
-    
+
     /**
      * Get all registered class IDs
      */
     public static Iterable<String> getAllClassIds() {
         return classes.keySet();
     }
-    
+
     /**
      * Initialize placeholder classes
      */
     public static void initializePlaceholderClasses() {
         // Warrior class
-        RPGClass warrior = new RPGClass("warrior", "Warrior", 
-            "A strong melee fighter with high health and damage", "warrior");
-        warrior.addBaseStat(StatType.MAX_HEALTH, new StatModifier("warrior_health", 20, StatModifier.Operation.ADD));
-        warrior.addBaseStat(StatType.DAMAGE, new StatModifier("warrior_damage", 5, StatModifier.Operation.ADD));
-        warrior.addBaseStat(StatType.DEFENSE, new StatModifier("warrior_defense", 3, StatModifier.Operation.ADD));
+        RPGClass warrior = new RPGClass("warrior", "Warrior",
+                "A strong melee fighter with high health and damage", "warrior");
+        warrior.addBaseStat(StatType.MAX_HEALTH, new StatModifier("warrior_health", StatType.MAX_HEALTH, 20.0, -1));
+        warrior.addBaseStat(StatType.DAMAGE, new StatModifier("warrior_damage", StatType.DAMAGE, 5.0, -1));
+        warrior.addBaseStat(StatType.DEFENSE, new StatModifier("warrior_defense", StatType.DEFENSE, 3.0, -1));
         register(warrior);
-        
+
         // Mage class
-        RPGClass mage = new RPGClass("mage", "Mage", 
-            "A powerful spellcaster with high mana and magical abilities", "mage");
-        mage.addBaseStat(StatType.MAX_MANA, new StatModifier("mage_mana", 50, StatModifier.Operation.ADD));
-        mage.addBaseStat(StatType.MANA_REGEN, new StatModifier("mage_regen", 2, StatModifier.Operation.ADD));
-        mage.addBaseStat(StatType.DAMAGE, new StatModifier("mage_damage", 3, StatModifier.Operation.ADD));
+        RPGClass mage = new RPGClass("mage", "Mage",
+                "A powerful spellcaster with high mana and magical abilities", "mage");
+        mage.addBaseStat(StatType.MAX_MANA, new StatModifier("mage_mana", StatType.MAX_MANA, 50.0, -1));
+        mage.addBaseStat(StatType.MANA_REGEN, new StatModifier("mage_regen", StatType.MANA_REGEN, 2.0, -1));
+        mage.addBaseStat(StatType.DAMAGE, new StatModifier("mage_damage", StatType.DAMAGE, 3.0, -1));
         register(mage);
-        
+
         // Rogue class
-        RPGClass rogue = new RPGClass("rogue", "Rogue", 
-            "A swift and agile fighter with high speed and critical hits", "rogue");
-        rogue.addBaseStat(StatType.MOVE_SPEED, new StatModifier("rogue_speed", 15, StatModifier.Operation.PERCENTAGE));
-        rogue.addBaseStat(StatType.ATTACK_SPEED, new StatModifier("rogue_attack", 20, StatModifier.Operation.PERCENTAGE));
-        rogue.addBaseStat(StatType.DAMAGE, new StatModifier("rogue_damage", 4, StatModifier.Operation.ADD));
+        RPGClass rogue = new RPGClass("rogue", "Rogue",
+                "A swift and agile fighter with high speed and critical hits", "rogue");
+        rogue.addBaseStat(StatType.MOVE_SPEED, new StatModifier("rogue_speed", StatType.MOVE_SPEED, 15.0, -1));
+        rogue.addBaseStat(StatType.ATTACK_SPEED, new StatModifier("rogue_attack", StatType.ATTACK_SPEED, 20.0, -1));
+        rogue.addBaseStat(StatType.DAMAGE, new StatModifier("rogue_damage", StatType.DAMAGE, 4.0, -1));
         register(rogue);
-        
+
         LOGGER.info("Initialized {} placeholder classes", classes.size());
     }
 }
